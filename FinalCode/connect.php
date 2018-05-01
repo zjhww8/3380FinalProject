623e9b5c20186848820b58cd604069d04bc105df
DlockMiz <daniel.w.lock@gmail.com>
2018-04-30 19:00:41 -0500

    database to php connection

--- FinalCode/connect.php
+++ FinalCode/connect.php
@@ -1,19 +1,13 @@
 <?php
-
-    $host = "database3380.crpnj5aroetm.us-east-1.rds.amazonaws.com";
-    $username = "root";
-    $password = "dalerocks";
-    $databaseName = "DaleDatabase";
-
-
-    $connect = mysqli_connect($host, $username, $password, $databaseName);
-
+    
+    include('database_creds.php');
+    
     $sql = "SELECT strings FROM test";
     $yuh = $connect->query($sql);
 
     while ($row = $yuh->fetch_assoc()) {
         echo $row['strings']."<br>";
     }
-    
+
     $connect->close();
 ?>
---
+++ FinalCode/database_creds.php
@@ -0,0 +1,8 @@
+<?php
+    $host = "database3380.crpnj5aroetm.us-east-1.rds.amazonaws.com";
+    $username = "root";
+    $password = "dalerocks";
+    $databaseName = "DaleDatabase";
+    
+    $connect = mysqli_connect($host, $username, $password, $databaseName);
+?>
